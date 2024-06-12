66065a957800c234c2d106b8b9e54ab6dfc859a3
AishaErel <146130819+AishaErel@users.noreply.github.com>
2024-06-12 14:25:26 -0700

    Update q_learning_agent.py

--- group project/q_learning_agent.py
+++ group project/q_learning_agent.py
@@ -3,12 +3,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # I worked on Google Collab environment initially, so there is no class names, etc
-#Assume that the area contains some obstacles, where agent cannot travel, and encourages agent to find fruit
-#After training, the game will run for 10 times, and will show how many steps does it take to find the fruit
 
+#Initialization part
 row_size = 11
 column_size = 11
-
 
 Q_values = np.zeros((row_size, column_size, 4))
 
@@ -18,10 +16,11 @@ actions = ['up', 'right', 'down', 'left']
 rewards = np.full((row_size, column_size), -100.) # a function that returns a new array of a given shape and type, filled with a -100
 rewards[0, 5] = 100. #set the reward for the food area, its going to be 100 when arrived that location
 
-#define locations where agent can travel (no obstacles)
-squares = {} #store locations in a dictionary
-squares[1] = [i for i in range(1, 10)]
-squares[2] = [1, 7, 9]
+#define locations  where agent can travel (out of range indicates obstacles, where agent is not allowed to be)
+squares = {} #store locations in a dictionary 
+squares[0] = [i for i in range(11)] #basically agent can travel through columns 1,2,3....10
+squares[1] = [i for i in range(1, 10)]  #basically agent can travel through columns 1,2,3....9
+squares[2] = [1, 7, 9]  #basically agent can travel through these columns: 1,7,9
 squares[3] = [i for i in range(1, 8)]
 squares[4] = [3, 7]
 squares[5] = [i for i in range(11)]
@@ -29,6 +28,7 @@ squares[6] = [5]
 squares[7] = [i for i in range(1, 10)]
 squares[8] = [3, 7]
 squares[9] = [i for i in range(11)]
+squares[10] = [i for i in range(11)]
 
 #set the rewards for all  locations 
 for row in range(1, 10):
@@ -92,7 +92,7 @@ def plot_steps(steps): #to keep track how many steps does it need to find the fr
     plt.show()
 
 
-def plot_performance(episodes, rewards):
+def plot_performance(episodes, rewards):  #to plot reward results
     plt.plot(episodes, rewards, label='Cumulative Reward')
     plt.xlabel('Episodes')
     plt.ylabel('Cumulative Reward')
@@ -101,34 +101,38 @@ def plot_performance(episodes, rewards):
     plt.show()
 
 
-def train_q_learning(epsilon=0.9, discount_factor=0.9, learning_rate=0.9, episodes=1000):
+def train_q_learning(epsilon=1.0, discount_factor=0.9, learning_rate=0.9, episodes=1000):
     episode_rewards = []
-    epsilon_decay = epsilon / episodes
+    epsilon_decay = epsilon / episodes #it will decrease later on
     
     for episode in range(episodes):
         row, column = start_location()
-        cumulative_reward = 0
+        cumulative_reward = 0 #initialize
         
         while not is_terminal_state(row, column):
-            action_index = next_action(row, column, epsilon)
-            old_row, old_column = row, column
-            row, column = next_location(row, column, action_index)
-            reward = rewards[row, column]
+        #calling next_action function, it will decide whether to explore(random) or exploit(best action)
+            action_index = next_action(row, column, epsilon) 
+            old_row, old_column = row, column  #current position agent is leaving from
+            row, column = next_location(row, column, action_index)  #updates the new position agent is moved to
+            reward = rewards[row, column] #retrieve the rewards based on the position
 
             cumulative_reward += reward
-            old_q_value = Q_values[old_row, old_column, action_index]
+            old_q_value = Q_values[old_row, old_column, action_index] #to get the current Q value(action, state) pair
+            
+          # use temporal difference formula, which is the difference between the predicted future reward and the current Q-value.
             temporal_difference = reward + (discount_factor * np.max(Q_values[row, column])) - old_q_value
-            new_q_value = old_q_value + (learning_rate * temporal_difference)
-            Q_values[old_row, old_column, action_index] = new_q_value
+            new_q_value = old_q_value + (learning_rate * temporal_difference) #update the Q-value
+            Q_values[old_row, old_column, action_index] = new_q_value #updated Q-value back into the Q-values matrix
         
-        episode_rewards.append(cumulative_reward)
+        episode_rewards.append(cumulative_reward)  #keeps track the total reward
 
         epsilon = max(epsilon - epsilon_decay, 0.1) # we can start decaying epsilon after it searches the environment sometime (exploration-exploiation)
     
+
     plot_performance(range(episodes), episode_rewards)
     print('Training completed!')
 
-def play_game(n_games = 10):
+def play_game(n_games = 10):  #define a function where agent will play 10 games by itself after training
       row, column = start_location()
       steps = 0
 
@@ -144,7 +148,7 @@ def play_game(n_games = 10):
 
 train_q_learning()
 
-#Play the game 10 times after training
+
 steps_to_find_fruit = []
 for game in range(10):
     steps = play_game()
